2f58cd03600b0f18f998800f80392f3c1e04e055
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-10-17 15:12:33 +0800

    added useSession in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 // import Image from "next/Image";
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+    const { data: session } = useSession();
     return (
         <main className="">
-            <p>Hello world!</p>
+            <p>Signed in as {session?.user?.name}</p>
         </main>
     );
 }
